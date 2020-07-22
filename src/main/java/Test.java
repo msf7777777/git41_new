4c70c2c3d6f7b712b56e1e2db631ebe78db26022
msf7777777 <82647191@qq.com>
2020-07-22 17:47:38 +0800

    第二次修改

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,8 @@
 public class Test {
     public static void main(String[] args) {
+        // 第一次提交
         System.out.println("Git need fuck_MSF");
+        // 第二次修改
+        System.out.println("麻少飞 want fuck the world！");
     }
 }
